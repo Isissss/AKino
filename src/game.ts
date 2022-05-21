4cd517acd8b66b801568440613578b5d06d38d3b
PimMiii <pim.milt@gmail.com>
2022-05-21 18:32:31 +0200

    updating circle

--- src/game.ts
+++ src/game.ts
@@ -7,41 +7,42 @@ import { Smog } from './smog'
 import { Graphics } from 'pixi.js'
 
 
+
 class Game {
     pixi: PIXI.Application
     loader: PIXI.Loader
     s: Smog
     graphics: Graphics
-    
+
 
     constructor() {
-        this.pixi = new PIXI.Application({ width: 800, height: 450, backgroundColor: 0x000000})
+        this.pixi = new PIXI.Application({ width: 800, height: 450, backgroundColor: 0x000000 })
         document.body.appendChild(this.pixi.view)
+        this.pixi.stage.interactive = true
         this.loader = new PIXI.Loader()
         this.loader.add('fishTexture', fishImage)
             .add('bubbleTexture', bubbleImage)
             .add('waterTexture', waterImage)
             .add('smogTexture', smokeImage)
         this.loader.load(() => this.loadCompleted())
+
     }
 
     loadCompleted() {
-        this.s = new Smog(this.pixi.screen.width/2,this.pixi.screen.height/2,100)
-        let graphics = new PIXI.Graphics()
-            .beginFill(0xFFFFFF)
-            .drawCircle(this.s.x, this.s.y, this.s.radius)
-            .endFill()
-        this.pixi.stage.addChild(graphics)
+        this.s = new Smog(this.pixi.screen.width/2, this.pixi.screen.height/2, 100)
+            
         
-        
+        this.pixi.stage.addChild(this.s)
+
+
         this.pixi.ticker.add((delta) => this.update(delta))
     }
 
     update(delta: number) {
         //console.log("Updating...")
-        this.s.update()
-        
+        this.s.update()    
         
     }
 }
+
 let g = new Game
--- src/smog.ts
+++ src/smog.ts
@@ -1,23 +1,51 @@
 import * as PIXI from 'pixi.js'
+import { InteractionData } from 'pixi.js'
 
 
-export class Smog{
+export class Smog extends PIXI.Graphics{
 
-    x:number
-    y:number
+    xPos:number
+    yPos:number
     radius:number
     
 
 
     constructor(x:number, y:number, radius:number){
-        this.x = x
-        this.y = y
+        super()
+        this.xPos = x
+        this.yPos = y
         this.radius = radius
+        this.interactive = true
+        this.draw()
+        
         
     }
 
-    update() {
-        this.radius --
+    draw() {
+        this.beginFill(0xffffff)
+        this.drawCircle(this.xPos,this.yPos,this.radius)
+        this.endFill
+
+    }
+
+    updatePos() {
+       
+    }
+
+    update(){
+        if(this.radius >= 1) {
+        this.radius -= 0.1
+        this.clear()
+        this.draw()
+        console.log(`radius: ${this.radius}`)
+        }
+        else{
+            console.log(`radius is already 0`)
+            console.log('resetting circle to 200 radius')
+            this.radius = 200
+            this.clear()
+            this.draw()
+        }
     }
 
 
