6cea403532d51a5d4fb58125907c6455da4b55a1
Isis <89838626+Isissss@users.noreply.github.com>
2022-05-24 22:04:29 +0200

    Update Player.ts

--- src/Player.ts
+++ src/Player.ts
@@ -2,13 +2,20 @@ import * as PIXI from 'pixi.js'
  
 
 export class Player extends PIXI.Sprite {
+    public speed: number 
 
     constructor(texture: PIXI.Texture) {
         super(texture)
     this.x = 790;
     this.y = 400;
+    this.speed = 4;
     this.anchor.set(0.5);
 
 
 }
+
+public update () {
+    this.x -= this.speed
 }
+}
+
